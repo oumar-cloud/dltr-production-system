61e97b8b60440c0bc45e1cb29d3bb1523c084e7e
Oumar Cloud <oumar.cisse@supdevinci-edu.fr>
2026-03-05 11:38:30 +0000

    database config

--- configs/settings.py
+++ configs/settings.py
@@ -93,13 +93,14 @@ WSGI_APPLICATION = 'configs.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+import dj_database_url
+import os
 
+DATABASES = {
+    'default': dj_database_url.config(
+        default=os.environ.get("DATABASE_URL")
+    )
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
